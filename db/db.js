ff993e90f9cfc9d828eef2149023f04a0e12cde6
William Weisberger <bill.weisberger@gmail.com>
2019-08-05 11:18:08 -0600

    Added .env file and heroku parameters

--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const connectionString = 'mongodb://localhost/coffee';
+const connectionString = process.env.MONGODB_URI;
 
 mongoose.connect(connectionString, {useNewUrlParser: true, useCreateIndex: true}, ()=>{
   console.log("connected to MONGOOOOOOSE")
--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.get('/', (req, res)=>{
   res.send("Base Page")
 });
 
-app.listen(9000, ()=>{
-  console.log("Listening on port 9000");
+app.listen(process.env.PORT, ()=>{
+  console.log("Listening on port " + process.env.PORT);
 });
